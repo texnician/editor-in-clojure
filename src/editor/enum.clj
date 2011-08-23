d4dc86d6f1f1332560dc0064de5ba44c2922e1c4
texnician <texnician@63d708db-85a2-4c44-859a-0191fc9c1613>
2011-08-23 09:23:26 +0000

    defenum 鍔熻兘瀹屾垚
    git-svn-id: http://svn.teamhost.org/server/trunk/editor/trunk@154 63d708db-85a2-4c44-859a-0191fc9c1613

--- src/editor/enum.clj
+++ src/editor/enum.clj
@@ -1,20 +1,6 @@
 (ns editor.enum
   (:use (editor domain)))
 
-(defenum asset-type-enum
-  "Asset类型枚举"
-  UNDEFINED
-  NPC
-  PLAYER
-  PLANTS)
-
-(contains? (into #{} (map #(-> % :attrs :name) (seq (((global-enum-domain) :asset-type-enum) :content)))) "LANTS")
-(register-global-enum :asset-type-enum {:tag :global-enum :attrs {:name "asset-type-enum" :doc "Asset类型枚举"}
-                                        :content [{:tag :item :attrs {:name "UNDEFINED"} :content ["0"]}
-                                                  {:tag :item :attrs {:name "NPC"} :content ["1"]}
-                                                  {:tag :item :attrs {:name "PLAYER"} :content ["2"]}
-                                                  {:tag :item :attrs {:name "PLANTS"} :content ["3"]}]})
-
 (declare fn-global-enum-contains?)
 
 (defmacro global-enum-contains? [enum sym]
@@ -41,5 +27,47 @@
   "Get enum value of sym."
   `((global-enum-map ~enum) ~(keyword sym)))
 
+
+(let [[v & k] [:a 1 2 3 4]]
+  (println v k))
 (defmacro defenum [name & body]
-  (register-global-enum ))
+  (let [name-str (str name)
+        [doc & enums] (if (string? (first body))
+                        body
+                        (conj "" body))]
+    `(register-global-enum ~(keyword name)
+                           (build-enum-body ~name-str ~doc ~@(map (fn [x]
+                                                                    `'~x) enums)))))
+
+(declare build-enum-items)
+
+(defn- build-enum-body [name doc & enums]
+  {:tag :global-enum
+   :attrs {:name name :doc doc}
+   :content (build-enum-items enums)})
+
+(defn- build-enum-items
+  ([enums] (build-enum-items [] 0 enums))
+  ([acc i enums]
+     (if (empty? enums)
+       acc
+       (recur (conj acc ((fn [sym n]
+                           {:tag :item :attrs {:name (str sym)} :content [(str n)]})
+                         (first enums) i))
+              (inc i)
+              (rest enums)))))
+
+(defenum asset-type-enum
+  "Asset类型枚举"
+  UNDEFINED
+  NPC
+  PLAYER
+  PLANTS
+  ITEMS
+  MAPS)
+
+(register-global-enum :asset-type-enum {:tag :global-enum :attrs {:name "asset-type-enum" :doc "Asset类型枚举"}
+                                        :content [{:tag :item :attrs {:name "UNDEFINED"} :content ["0"]}
+                                                  {:tag :item :attrs {:name "NPC"} :content ["1"]}
+                                                  {:tag :item :attrs {:name "PLAYER"} :content ["2"]}
+                                                  {:tag :item :attrs {:name "PLANTS"} :content ["3"]}]})
