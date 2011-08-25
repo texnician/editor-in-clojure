f6a16468ee2afe78134519c3e8f2c9547c7eb94d
texnician <texnician@63d708db-85a2-4c44-859a-0191fc9c1613>
2011-08-25 08:22:08 +0000

    component utility
    git-svn-id: http://svn.teamhost.org/server/trunk/editor/trunk@159 63d708db-85a2-4c44-859a-0191fc9c1613

--- src/editor/component.clj
+++ src/editor/component.clj
@@ -26,3 +26,60 @@
     (if (empty? l)
       acc
       (recur (conj acc (build-attribute (first l))) (rest l)))))
+
+;; [{:tag :go-attribute, :attrs {:name "id", :type "int", :default 0, :doc "Domain内的唯一id"}}
+;;              {:tag :go-attribute, :attrs {:name "name", :type "string", :default "(unamed object)", :doc "Object的名字"}}]
+
+;; {:id {:type "int"
+;;       :default 0
+;;       :doc "Domain内的唯一id"}
+;;  :name {:type "string"
+;;         :default "(unamed object)"
+;;         :doc "Object的名字"}}
+
+(defn make-meta-attributes [meta-attribute-node]
+  "根据meta-attribute-node生成meta-attributes"
+  (if-let [attr-maps (map :attrs meta-attribute-node)]
+    (into {} (map (fn [entry]
+                    [(keyword (entry :name)) (dissoc entry :name)])
+                  attr-maps))))
+
+;; {:content [{:tag :go-attribute, :attrs {:name "id", :type "int", :default 0, :doc "Domain内的唯一id"}} {:tag :go-attribute, :attrs {:name "name", :type "string", :default "(unamed object)", :doc "Object的名字"}}], :attrs {:name "base", :doc "Game object基本组件"}, :tag :go-component}
+
+;; {:name "base"
+;;  :doc "Object的名字"
+;;  :attributes {:id {:type "int"
+;;                    :default 0
+;;                    :doc "Domain内的唯一id"}
+;;               :name {:type "string"
+;;                      :default "(unamed object)"
+;;                      :doc "Object的名字"}}}
+
+(defn make-meta-comp [meta-node]
+  "根据go-component-domain中的component node生成一个meta component"
+  (assoc (meta-node :attrs) :attributes (make-meta-attributes (meta-node :content))))
+
+;; {:tag :go-component
+;;             :attrs {:name "base" :doc "基本组件"}
+;;             :content [{:tag :id
+;;                        :attrs {:type "int" :doc "Domain内的唯一id"}
+;;                        :content ["0"]}
+;;                       {:tag :name
+;;                        :attrs {:type "string" :doc "Object的名字"}
+;;                        :content ["(Unamed)"]}]}
+
+(defn make-component-node [comp-key & attrs]
+  "生成一个component节点"
+  (if-let [comp-meta-node ((go-component-domain) comp-key)]
+    (let [comp-meta (make-meta-comp comp-meta-node)
+          meta-attrs (comp-meta :attributes)]
+      {:tag :go-component
+       :attrs {:name (comp-meta :name) :doc (comp-meta :doc)}
+       :conent (map (fn [x]
+                      {:tag (first x)
+                       :attrs (second x)
+                       :content
+                       (if-let [v ((second x) :default)]
+                         [(str v)]
+                         nil)
+                       }) meta-attrs)})))
