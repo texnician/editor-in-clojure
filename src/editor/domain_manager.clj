4b36210478d6d466aa7924266629e3ed5ca31a75
texnician <texnician@63d708db-85a2-4c44-859a-0191fc9c1613>
2011-08-30 11:04:39 +0000

    export xml
    git-svn-id: http://svn.teamhost.org/server/trunk/editor/trunk@170 63d708db-85a2-4c44-859a-0191fc9c1613

---
+++ src/editor/domain_manager.clj
@@ -0,0 +1,32 @@
+(ns editor.domain-manager
+  (:use (editor domain go-template go-component))
+  (:import (java.io File)))
+
+;;; default xml output dir
+(def *xml-output-dir* ".")
+
+(defn get-all-go-domain-name []
+  "返回所有的game object keyword列表"
+  (keys (get-domain :go-template)))
+
+(defn domain-name->filename [dir-name suffix]
+  (fn [name-key]
+    (let [dir (File. dir-name)
+          path (.getAbsolutePath dir)]
+      (apply str (concat path File/separator (name name-key) "." suffix)))))
+
+(defn output-domian-to-xml-file [domain-key]
+  (let [dir (File. *xml-output-dir*)]
+    (if-not (.exists dir)
+      (.mkdirs dir))
+    (domain->xml-file ((domain-name->filename (.getAbsolutePath dir) "xml") domain-key) domain-key)))
+
+(defn all-go-domain->xml-file []
+  (doall (map output-domian-to-xml-file (get-all-go-domain-name))))
+
+
+(defn all-go-template->xml-file []
+  (output-domian-to-xml-file :go-template))
+
+(defn all-meta-component->xml-file []
+  (output-domian-to-xml-file :meta-go-component))
--- src/editor/main.clj
+++ src/editor/main.clj
@@ -1,13 +1,14 @@
 (ns editor.main
   (:use (clojure pprint))
-  (:use (editor core domain go-template))
+  (:use (editor core domain go-template domain-manager))
   (:gen-class))
 
 (defn -main [& args]
   (println "Welcome to happy world editor! These are your args:" args)
   (let [files (map #(str %) args)]
     (doall (map load-file files)))
-  (domain->xml-file "fruit.xml" :fruit)
-  (domain->xml-file "seed.xml" :seed)
   (print1 (get-domain :fruit))
-  (print1 (get-domain :seed)))
+  (print1 (get-domain :seed))
+  (all-meta-component->xml-file)
+  (all-go-template->xml-file)
+  (all-go-domain->xml-file))
