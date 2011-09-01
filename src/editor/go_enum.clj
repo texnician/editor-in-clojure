5946f0c1b109376343d2a901e1993c9cb4e5330a
Tang Yaguang <texncian@gmail.com>
2011-09-02 01:12:06 +0800

    add monster example

--- src/editor/go_enum.clj
+++ src/editor/go_enum.clj
@@ -2,12 +2,13 @@
   (:use (editor enum)))
 
 (defenum monster-species-enum
-  UNDEFINED-SPECIES
-  METAL
-  SLIME
-  DRAGON
-  NATURAL
-  ORC
-  SUBSTANCE
-  DEMON
-  ZOMBIE)
+  UNDEFINED-SPECIES                     ;未定义物种
+  METAL                                 ;金属类
+  SLIME                                 ;史莱姆
+  DRAGON                                ;龙
+  NATURAL                               ;自然
+  ORC                                   ;魔兽
+  SUBSTANCE                             ;物质
+  DEMON                                 ;恶魔
+  ZOMBIE                                ;僵尸
+  )
