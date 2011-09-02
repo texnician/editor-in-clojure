aa04c2a78c1efdf469edfcefb75db54b0358f5f5
tangyaguang <tangyaguang@63d708db-85a2-4c44-859a-0191fc9c1613>
2011-09-02 02:19:54 +0000

    add inspect util
    git-svn-id: http://svn.teamhost.org/server/trunk/editor/trunk@197 63d708db-85a2-4c44-859a-0191fc9c1613

--- src/editor/inspect_util.clj
+++ src/editor/inspect_util.clj
@@ -1,14 +1,14 @@
-(ns inspect-util
+(ns editor.inspect-util
   (:use (editor core template domain))
   (:require (clojure inspector)))
 
-(defn- inspect-template-fn [name-key]
+(defn inspect-template-fn [name-key]
   (clojure.inspector/inspect-table (make-inspect-template name-key)))
 
 (defmacro inspect-template [name]
   `(inspect-template-fn ~(keyword name)))
 
-(defn- inspect-object-fn [domain-key id]
+(defn inspect-object-fn [domain-key id]
   (clojure.inspector/inspect-table (make-inspect-game-object domain-key id)))
 
 (defmacro inspect-object [domain id]
