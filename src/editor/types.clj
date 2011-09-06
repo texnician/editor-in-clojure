ed8f2f4632f773a53d1d52a903da8ca4bd199355
tangyaguang <tangyaguang@63d708db-85a2-4c44-859a-0191fc9c1613>
2011-09-06 11:00:35 +0000

    cpp types multimethod
    git-svn-id: http://svn.teamhost.org/server/trunk/editor/trunk@210 63d708db-85a2-4c44-859a-0191fc9c1613

---
+++ src/editor/types.clj
@@ -0,0 +1,70 @@
+(ns editor.types
+  (:use (editor core component name-util)))
+
+(defn get-attribute-type [comp-key attr-key]
+  "返回一个attribute的类型"
+  (let [type-info (get-attribute-meta-info comp-key attr-key #{:type})]
+    (keyword (:type type-info))))
+
+(defn get-attribute-default-value [comp-key attr-key]
+  "返回一个attribute的default value"
+  (let [type-info (get-attribute-meta-info comp-key attr-key #{:default})]
+    (str (:default type-info))))
+
+(defmulti getter-return-type (fn [comp-key attr-key lang]
+                               [(get-attribute-type comp-key attr-key) lang]))
+
+(defmethod getter-return-type [:int :cpp] [comp-key attr-key lang]
+  "int")
+
+(defmethod getter-return-type [:string :cpp] [comp-key attr-key lang]
+  "const std::string&")
+
+(defmethod getter-return-type [:enum :cpp] [comp-key attr-key lang]
+  "int")
+
+(defmethod getter-return-type [:bool :cpp] [comp-key attr-key lang]
+  "bool")
+
+(defmulti attribute-member-name (fn [attr lang] lang))
+(defmethod attribute-member-name :cpp [attr lang] (str (name attr) \_))
+
+(defmulti setter-argument-type (fn [comp-key attr-key lang]
+                                 [(get-attribute-type comp-key attr-key) lang]))
+
+(defmethod setter-argument-type [:int :cpp] [comp-key attr-key lang]
+  "int")
+
+(defmethod setter-argument-type [:string :cpp] [comp-key attr-key lang]
+  "const std::string&")
+
+(defmethod setter-argument-type [:enum :cpp] [comp-key attr-key lang]
+  "int")
+
+(defmethod setter-argument-type [:bool :cpp] [comp-key attr-key lang]
+  "bool")
+
+(defmulti attribute-default-value (fn [comp-key attr-key lang]
+                                    [(get-attribute-type comp-key attr-key) lang]))
+
+(defmethod attribute-default-value [:int :cpp] [comp-key attr-key lang]
+  (get-attribute-default-value comp-key attr-key))
+
+(defmethod attribute-default-value [:string :cpp] [comp-key attr-key lang]
+  (get-attribute-default-value comp-key attr-key))
+
+(defmethod attribute-default-value [:bool :cpp] [comp-key attr-key lang]
+  (get-attribute-default-value comp-key attr-key))
+
+(defmethod attribute-default-value [:enum :cpp] [comp-key attr-key lang]
+  (clojure-token->cpp-enum-token (get-attribute-default-value comp-key attr-key)))
+
+
+(defn make-cpp-attribute [comp-key attr-key]
+  {:member-name (attribute-member-name attr-key :cpp)
+   :getter-return-type (getter-return-type comp-key attr-key :cpp)
+   :getter-name (cpp-getter-name attr-key)
+   :setter-name (cpp-setter-name attr-key)
+   :setter-argument-type (setter-argument-type comp-key attr-key :cpp)
+   :setter-argument-name (name attr-key)
+   :default-value (attribute-default-value comp-key attr-key :cpp)})
