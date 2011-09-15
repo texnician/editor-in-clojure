95f31d1ba70b15e7c6028bdbf2dc47a41314d637
tangyaguang <tangyaguang@63d708db-85a2-4c44-859a-0191fc9c1613>
2011-09-15 02:15:22 +0000

    add cli interface
    git-svn-id: http://svn.teamhost.org/server/trunk/editor/trunk@231 63d708db-85a2-4c44-859a-0191fc9c1613

--- src/editor/main.clj
+++ src/editor/main.clj
@@ -3,9 +3,62 @@
   (:use (editor core domain go-template domain-manager inspect-util st))
   (:gen-class))
 
-(defn -main [& args]
-  (let [files (map #(str %) args)]
-    (doall (map load-file files)))
-  (all-meta-component->xml-file)
-  (all-go-template->xml-file)
+(defn- compile-xml [& files]
+  (doseq [f files]
+    (load-file f))
   (all-go-domain->xml-file))
+
+(declare *opt-table*)
+
+(defn- usage [& args]
+  (println "Welcome to happy world editor.")
+  (println "Usage: java -c editor-x.x.x-.jar [option] [arg] ...")
+  (doseq [[k v] *opt-table*]
+    (println (format "   %s    %s" (name k) (:doc v)))))
+
+(defn- list-components [& args]
+  (let [comp-list (keys (get-domain :meta-go-component))]
+    (doseq [c comp-list]
+      (-> c name println))))
+
+(defn- list-templates [& args]
+  (let [temp-list (keys (get-domain :go-template))]
+    (doseq [t temp-list]
+      (-> t name println))))
+
+(defn- list-domains [& args]
+  (let [domain-list (keys (deref *global-domain*))]
+    (doseq [d domain-list]
+      (-> d name println))))
+
+(defn- inspect-template-by-name [arg]
+  (inspect-template-fn (keyword arg)))
+
+(defn- generate-cpp [& args]
+  (let [comp-list (keys (get-domain :meta-go-component))]
+    (doseq [c comp-list]
+      (gen-component c))
+    (gen-component-factory comp-list)
+    (gen-component-factory-test comp-list)))
+
+(def *opt-table*
+  {:-c {:func compile-xml :doc "Compile domain scripts, output xml data files."}
+   :-C {:func generate-cpp :doc "Generate cpp srcs."}
+   :-d {:func list-domains :doc "List all domains."}
+   :-h {:func usage :doc "Print this message."}
+   :-i {:func inspect-template-by-name :doc "Inspect template"}
+   :-l {:func list-components :doc "List all components"}
+   :-L {:func list-templates :doc "List all templates"}})
+
+(defn parse-args [arg-list]
+  (let [opt (first arg-list)
+        args (next arg-list)]
+    (fn []
+      (apply (:func (*opt-table* (keyword opt))) args))))
+
+(defn -main [& args]
+  (try (let [f (parse-args args)]
+         (f))
+       (catch Exception e (binding [*out* *err*]
+                            (println (.getMessage e))
+                            (usage)))))
