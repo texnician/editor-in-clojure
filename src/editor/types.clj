d628ebf8645714331a3cf5e6c92ad893d69396d1
Tang Yaguang <texncian@gmail.com>
2011-10-18 00:04:20 +0800

    remove unused line

--- src/editor/types.clj
+++ src/editor/types.clj
@@ -21,7 +21,6 @@
 (defmethod define-type [:bool :cpp] [comp-key attr-key lang]
   (cpp-define-type comp-key attr-key "bool"))
 
-(defn- cpp-getter-return-type )
 (defmulti getter-return-type (fn [comp-key attr-key lang]
                                [(get-attribute-type comp-key attr-key) lang]))
 
