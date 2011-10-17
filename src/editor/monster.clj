4b4367ca6aa20741434f6c5472e508c599c47bce
tangyaguang <tangyaguang@63d708db-85a2-4c44-859a-0191fc9c1613>
2011-10-17 02:31:54 +0000

    cpp array type xml support
    git-svn-id: http://svn.teamhost.org/server/trunk/editor/trunk@297 63d708db-85a2-4c44-859a-0191fc9c1613

--- src/editor/monster.clj
+++ src/editor/monster.clj
@@ -3,16 +3,16 @@
 
 (defmonster :id 1 :name "史莱姆1号" :level 3 :hp 300 :mp 800  :species SLIME
   :monster-rank "F" :generation 8 :summon-cost 1 :attack 80 :defence 70
-  :speed 1 :mental 5 :dodge-rate 2000 :crit-rate 1500 :magic-resistance 8)
+  :speed 1 :mental 5 :dodge-rate 2000 :crit-rate 1500 :magic-resistance [1 2 3])
 
 (defmonster :id 2 :name "机器人" :level 10 :hp 1000 :mp 1000 :species "METAL" 
   :monster-rank "A" :generation 2 :summon-cost 2 :attack 100 :defence 150
-  :speed 4 :mental 6 :dodge-rate 2500 :crit-rate 2000 :magic-resistance 20)
+  :speed 4 :mental 6 :dodge-rate 2500 :crit-rate 2000 :magic-resistance [])
 
 (defmonster :level 6 :id 3 :name "恶魔犬" :hp 500 :mp 500 :species DEMON
   :monster-rank SS :generation 8 :summon-cost 3 :attack 40 :defence 100
-  :speed 3 :mental 10 :dodge-rate 2000 :crit-rate 1000 :magic-resistance 70)
+  :speed 3 :mental 10 :dodge-rate 2000 :crit-rate 1000 :magic-resistance [70])
 
 (defmonster :level 5 :id 4 :name "XXX" :hp 500 :mp 500 :species DEMON
   :monster-rank SS :generation 8 :summon-cost 3 :attack 40 :defence 100
-  :speed 3 :mental 10 :dodge-rate 2000 :crit-rate 1000 :magic-resistance 70)
+  :speed 3 :mental 10 :dodge-rate 2000 :crit-rate 1000 :magic-resistance [80])
