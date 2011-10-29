2699af8793c461238e20d9161a8b5b9b3df766bb
Tang Yaguang <texnician@gmail.com>
2011-10-29 14:50:57 +0800

    add test case for scene-object component

--- src/editor/unit_test.clj
+++ src/editor/unit_test.clj
@@ -43,6 +43,7 @@
    :seeding (deffactory-test :seeding)
    :item-base (deffactory-test :item-base)
    :role-info (deffactory-test :role-info)
-   :currency (deffactory-test :currency)})
+   :currency (deffactory-test :currency)
+   :scene-object (deffactory-test :scene-object {:scene-object-position 11})})
 
 ;(deffactory-test :combat-property {:attack 80 :defence 99 :speed 7 :mental 29 :dodge-rate 73 :crit-rate 1500 :magic-resistance 0x2C})
