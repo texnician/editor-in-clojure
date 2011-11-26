e3ff1e88988f5693b2ae0c8ab59553a6916f06cd
unknown <yingle@sh-yingle.SNDA.ROOT.CORP>
2011-11-26 11:16:15 +0800

    add avater.clj

---
+++ src/editor/avatar.clj
@@ -0,0 +1,15 @@
+(ns editor.avartar
+  (:use (editor go-template template)))
+
+(definsect :id 90001 :name "爆炸头" :pic "baozhatou.png" :suit 0 :buy-price 600 :tips "热血爆炸头" )
+
+(definsect :id 90002 :name "中分发型" :pic "fentou.png" :suit 0 :buy-price 600 :tips "中分的发型" )
+
+(definsect :id 80001 :name "快乐冒险服" :pic "maoxianfu.png" :suit 1 :buy-price 600 :tips "初级冒险者的服装")
+
+(definsect :id 80002 :name "摇滚服" :pic "yaogunfu.png" :suit 2 :buy-price 300 :tips "摇滚明星参加表演的服装") 
+
+(definsect :id 70001 :name "快乐冒险鞋" :pic "maoxianxie.png" :suit 1 :buy-price 300 :tips "初级冒险者的鞋子") 
+
+(definsect :id 70002 :name "摇滚鞋" :pic "yaogunxie.png" :suit 2 :buy-price 300 :tips "摇滚明星参加表演的鞋子") 
+
